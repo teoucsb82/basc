076ed9fefc6c8a41851699790a1d0fbd3f75e213
Teo Dell'Amico <teo.dellamico@gmail.com>
2017-01-29 22:52:19 -0800

    Added basic crud for tickets linked to events and users

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,14 @@
 class TicketsController < ApplicationController
-  
+  before_filter :authenticate_user!
+  before_filter :set_user
+  before_filter :set_event, only: [:new, :create, :show]
+
+  # GET /tickets
+  # GET /tickets.json
+  def index
+    @tickets = @user.tickets
+  end
+
   # GET /tickets/1
   # GET /tickets/1.json
   def show
@@ -18,7 +27,7 @@ class TicketsController < ApplicationController
 
     respond_to do |format|
       if @ticket.save
-        format.html { redirect_to @ticket, notice: 'ticket was successfully created.' }
+        format.html { redirect_to event_ticket_path(@event, @ticket), notice: 'ticket was successfully created.' }
         format.json { render :show, status: :created, location: @ticket }
       else
         format.html { render :new }
@@ -28,9 +37,16 @@ class TicketsController < ApplicationController
   end
 
   private
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def set_user
+    @user = current_user
+  end
 
   # Never trust parameters from the scary internet, only allow the white list through.
-  def event_params
-    params.require(:ticket).permit(:event_id)
+  def ticket_params
+    params.require(:ticket).permit(:event_id).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     resources :events
   end
 
-  resources :events, only: [:index, :show]
-  resources :tickets, only: [:new, :create, :show]
+  resources :events, only: [:index, :show] do
+    resources :tickets, only: [:new, :create, :show]
+  end
+  resources :tickets, only: [:index]
   get 'pages/home'
 end
